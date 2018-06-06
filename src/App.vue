682ed75feef66a55b48e1e5a92582602e999ba80
eyrad4 <eyrad4@gmail.com>
2018-06-06 14:49:51 +0300

    add basic html for app with header and navigations

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,101 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      temporary
+      fixed
+      v-model="sideNav">
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              <router-link to="/" tag="span" style="cursor: pointer">
+                {{ siteName }}
+              </router-link>
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list dense class="pt-0">
+        <v-list-tile
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile
+        >
+          <v-list-tile-action>
+            <v-icon>exit_to_app</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>Logout</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="teal darken-3">
+      <v-toolbar-side-icon
+        @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">
+          {{ siteName }}
+        </router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn
+          flat
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
+          <v-icon left>{{ item.icon }}</v-icon>
+          {{ item.title }}
+        </v-btn>
+        <v-btn
+          flat
+        >
+          <v-icon left>exit_to_app</v-icon>
+          Logout
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      <router-view/>
+    </main>
+    <v-footer class="pa-3">
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    sideNav: false,
+    siteName: 'SiteName'
+  }),
+  computed: {
+    menuItems () {
+      let menuItems = [
+        { icon: 'face', title: 'Sign up', link: 'register' },
+        { icon: 'lock_open', title: 'Sign in', link: 'login' },
+        { icon: 'add', title: 'Add ads', link: 'new' },
+        { icon: 'folder', title: 'All categories', link: 'lists' },
+        { icon: 'person', title: 'Profile admin', link: 'admin' }
+      ]
+      return menuItems
+    }
+  },
+  methods: {
+  }
 }
 </script>
