865b8f9ecc680adf7897bd356b3840c31a063923
eyrad4 <eyrad4@gmail.com>
2018-06-06 14:48:42 +0300

    same settings and install vuetify and axios

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import store from '@/store'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
