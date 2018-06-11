778107c4b8c5fa7b5cfd2b53483d626f656bdde9
eyrad4 <eyrad4@gmail.com>
2018-06-11 14:33:56 +0300

    add new route

--- src/store/api.js
+++ src/store/api.js
@@ -2,5 +2,8 @@ const HTTP = process.env.ROOT_API
 export default {
   signup: HTTP + 'register',
   signin: HTTP + 'login',
-  logout: HTTP + 'logout'
+  logout: HTTP + 'logout',
+  dataConfig: HTTP + 'alldata',
+  createAd: HTTP + 'create',
+  showAd: HTTP + 'product/'
 }
