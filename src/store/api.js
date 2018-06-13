bff07c0837d0e26859b6ed75d191ee8d9c75fb61
eyrad4 <eyrad4@gmail.com>
2018-06-13 16:37:49 +0300

    add api

--- src/store/api.js
+++ src/store/api.js
@@ -1,9 +1,12 @@
 const HTTP = process.env.ROOT_API
 export default {
+  index: HTTP + 'index',
   signup: HTTP + 'register',
   signin: HTTP + 'login',
   logout: HTTP + 'logout',
   dataConfig: HTTP + 'alldata',
   createAd: HTTP + 'create',
-  showAd: HTTP + 'product/'
+  showAd: HTTP + 'product/',
+  adsList: HTTP + 'products',
+  search: HTTP + 'search/'
 }
